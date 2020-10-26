e21763e155dc7957e6ac51d148087f7c55a0b96a
Erikgach <69018839+Erikgach@users.noreply.github.com>
2020-10-26 17:56:48 -0600

    Se Inicia el proyecto

---
+++ poblacion/poblacion/src/test/java/org/una/poblacion/PoblacionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.poblacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoblacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
